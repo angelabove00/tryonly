01fadafacfb03a590f6021749818a8f2100a2ab6
appanlilio <lr.apanlilio@mmdc.mcl.edu.ph>
2025-05-19 08:16:18 +0900

    Update Main.java for UI Implementation

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,7 @@ import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
-        Employee emp = new Employee();
-        emp.setName("Juan Dela Cruz");
-        emp.setEmployeeNumber("EMP001");
-        emp.setContactInfo("09171234567");
-        emp.setBirthday(1991,12,3);
-        System.out.println("Employee: " + emp.getName() + ", ID: " + emp.getEmployeeNumber() + ", Born: " + emp.getBirthday());
-
-        Attendance att = new Attendance();
-        att.setDate(LocalDate.now());
-        att.setTimeIn(LocalTime.of(8, 0));
-        att.setTimeOut(LocalTime.of(17, 30));
-        System.out.println("Hours worked: " + att.calculateTotalHours());
-
-        Deductions ded = new Deductions();
-        ded.setSss(100); ded.setPhilHealth(50);
-        ded.setPagIbig(30); ded.setWithholdingTax(200);
-        ded.setOtherDeductions(20);
-
-        Payroll pay = new Payroll();
-        pay.setTotalHoursWorked(att.calculateTotalHours());
-        pay.setOvertimeHours(1.5);
-        pay.setBonuses(200);
-        pay.setDeductions(ded);
-        System.out.println("Gross Salary: " + pay.calculateGrossSalary() + " PHP");
-        System.out.println("Net Salary:   " + pay.calculateNetSalary() + " PHP");
-        
-        LoginSession sid = new LoginSession();
-        sid.setLoginTime(LocalTime.now());
-        System.out.println("\n\n\nLogin Time: " + sid.getLoginTime());
+        MainInterface my = new MainInterface();
+        my.setVisible(true);
     }
 }
